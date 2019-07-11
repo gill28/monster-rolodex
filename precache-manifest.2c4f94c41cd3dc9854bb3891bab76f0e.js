ab41875d6a2b822c4dce9bd98f0764da64407c7e
ngill28 <35106903+ngill28@users.noreply.github.com>
2019-07-11 10:38:51 -0500

    Updates

---
+++ precache-manifest.2c4f94c41cd3dc9854bb3891bab76f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5954fbb2396be16cb83f0c23564002c8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2e27a54447ef9b8aa52d",
+    "url": "/monster-rolodex/static/css/main.c31d453e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2e27a54447ef9b8aa52d",
+    "url": "/monster-rolodex/static/js/main.038f7f9a.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
